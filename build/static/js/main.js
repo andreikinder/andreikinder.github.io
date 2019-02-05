7ed1b2e76f0b95adcbba05b32f66c33030afd9f6
andreikinder <42671896+AndreiKinder@users.noreply.github.com>
2019-02-05 19:16:24 +0500

    sidebar

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
     svg4everybody({});
+
+   
+    // Slideout menu
+	var slideout = new Slideout({
+		'panel': document.getElementById('panel'),
+		'menu': document.getElementById('menu'),
+	 	'side': 'left'
+	});
+	// Toggle button
+      document.querySelector('.btn-sideNav').addEventListener('click', function() {
+        slideout.toggle();
+     });
+
+    //Close  button
+     $('.slide_close').click(function(event) {
+     	 slideout.close();
+     });
+	
+
+
 });
