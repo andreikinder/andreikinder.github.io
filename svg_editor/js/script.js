6aaac09cead13aafd3e339d676a0d379c3b6e3c3
andreikinder <42671896+AndreiKinder@users.noreply.github.com>
2019-11-19 01:09:22 +0500

    f

--- svg_editor/js/script.js
+++ svg_editor/js/script.js
@@ -14,6 +14,19 @@ jQuery(document).ready(function($) {
 
 
 //range slider
-
+	$('.counter__minus').click(function () {
+		var $input = $(this).parent().find('input');
+		var count = parseInt($input.val()) - 1;
+		count = count < 0 ? 0 : count;
+		$input.val(count);
+		$input.change();
+		return false;
+	});
+	$('.counter__plus').click(function () {
+		var $input = $(this).parent().find('input');
+		$input.val(parseInt($input.val()) + 1);
+		$input.change();
+		return false;
+	});
 
 });
